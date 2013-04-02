880f3ef22cc50e6e60bfe5aa7eec3cba5b9e1873
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-04-02 13:37:15 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsPrelaunchSignup::Application.config.secret_token = 'f55710b68f826d20c2160aed8c956b46b960987cd9468bd20ac6f6731260c71d53576cf5c81da0cca372927db9b79d7f167763589e967eb99933bd7c2ab4d232'
+RailsPrelaunchSignup::Application.config.secret_token = '1d7c735df85efae6c5355579b800fc5949ccc237f7ae6ec32c56c6fb7a27351064c6189898c5c8ed993c92e6c550964706e5dab00b8ef2610a9f7d973482d6f8'
