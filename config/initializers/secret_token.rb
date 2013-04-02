f911873f66911f3d3e3d342804d37669b7f5a8b2
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-04-02 12:54:06 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPrelaunchSignup::Application.config.secret_token = '376f7e88c13f5ea9375238fa8813e3beae264242c5daead7e814a35720b6d46c358d6f2d1564148c7eb6c3d4dbe1af42f38b1da40c291f65c3c9197956d4451d'
